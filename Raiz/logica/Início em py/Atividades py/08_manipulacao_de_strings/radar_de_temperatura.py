9816b44f6cf15cd5dbd74f634096beb209e9ccdb
OTAVIO MIGUEL FERNANDES DE ARAUJO <20261181110031@ifrn.local>
2026-09-25 07:32:11 -0300

    funcao

---
+++ Raiz/logica/Início em py/Atividades py/08_manipulacao_de_strings/radar_de_temperatura.py
@@ -0,0 +1,13 @@
+matriz_temperaturas = [
+    [28.5, 30.1, 31.2, 29.0],  
+]
+
+regioes = ["Norte", "Nordeste", "Sudeste"]
+trimeses = ["Trimestre 1", "Trimestre 2", "Trimestre 3", "Trimestre 4"]
+
+
+
+
+
+
+
---
+++ Raiz/logica/Início em py/Atividades py/09_funcoes/funcoes.py
@@ -0,0 +1,4 @@
+def saudar(nome):
+    print(f"Olá, {nome}!")
+
+saudar(input("Digite seu nome: "))
